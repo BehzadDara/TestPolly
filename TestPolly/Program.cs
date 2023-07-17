7114e8d056b789040b294bcd2ba9e482e44fc8cc
behzad dara <Behzad.dara.99@gmail.com>
2023-07-17 18:36:57 +0330

    Add project files.

---
+++ TestPolly/Program.cs
@@ -0,0 +1,27 @@
+﻿using Polly;
+var counter = 0;
+
+var policy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(
+                    10,
+                    retryAttempt => TimeSpan.FromSeconds(2 * retryAttempt),
+                    (exception, timeSpan, retryCount, context) =>
+                    {
+                        Console.WriteLine($"{DateTime.Now:dd/MM/yyyy} - {DateTime.Now:HH:mm:ss}" +
+                            $" , Attempt {retryCount + 1} , Message = {exception.Message}");
+                    }
+                );
+
+await policy.ExecuteAsync(async () => await DoAction(ref counter));
+
+static Task DoAction(ref int counter)
+{
+    if (counter <= 3)
+    {
+        counter++;
+        throw new Exception();
+    }
+    Console.WriteLine($"Action Done at : {DateTime.Now:dd/MM/yyyy} - {DateTime.Now:HH:mm:ss}");
+    return Task.CompletedTask;
+}
